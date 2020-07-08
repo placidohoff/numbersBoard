2bea7174caf59414f681e71a4dc511673e6c5918
placidohoff <sweetflow401@gmail.com>
2020-07-08 19:31:18 -0400

    Initial Commit, Game Height fits all Screen Resize

---
+++ game.js
@@ -0,0 +1,30 @@
+let game;
+window.onload = function(){
+    let gameConfig = {
+        width: 480,
+        height: 640,
+        backgroundColor: 0xff0000
+    }
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+    resizeGame();
+    window.addEventListener("resize", resizeGame);
+
+}
+
+//This functionality makes the game fill the whole height of the window regardless of manual resize:
+    //NOTE: This only works as should on google chrome and not microsoft edge
+function resizeGame(){
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + 'px';
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
